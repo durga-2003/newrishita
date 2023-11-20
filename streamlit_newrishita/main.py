cde60965b1d6af046d6ae1c7c9fee46704356904
durga-2003 <131599952+durga-2003@users.noreply.github.com>
2023-11-20 15:13:07 +0530

    Update main.py

--- streamlit_newrishita/main.py
+++ streamlit_newrishita/main.py
@@ -135,7 +135,7 @@ try:
                         x_test = []
                         y_test = []
 
-                    for i in range(100 , input_data.shape[0]):
+                for i in range(100 , input_data.shape[0]):
                             x_test.append(input_data[i-100:i])
                             y_test.append(input_data[i,0])
                         x_test, y_test = np.array(x_test), np.array(y_test)
